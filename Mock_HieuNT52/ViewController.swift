adab2fb17fd53bf485dc8bbbd6efd055483e9486
hasam0703 <hasam0703@gmail.com>
2017-02-21 13:49:14 +0700

    commit to remove warning missing file

--- Mock_HieuNT52/ViewController.swift
+++ Mock_HieuNT52/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
     }
 
     override func didReceiveMemoryWarning() {
